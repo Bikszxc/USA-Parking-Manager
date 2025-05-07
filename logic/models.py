9f2bc9914573816ce1e634bf81860c2c14c9996f
Bikzxcs <reagan2sotelo@gmail.com>
2025-05-07 09:40:42 +0800

    added fetch for car owners

--- logic/models.py
+++ logic/models.py
@@ -6,7 +6,7 @@ cursor = conn.cursor()
 # Add New Car Owner
 def new_car_owner(owner_name, email, owner_type, contact_number):
     try:
-        cursor.execute("INSERT INTO car_owners (owner_name, email, owner_type, contact_number) VALUES (?, ?, ?, ?)",
+        cursor.execute("INSERT INTO car_owners (owner_name, email, type, contact_number) VALUES (?, ?, ?, ?)",
                        (owner_name, email, owner_type, contact_number))
 
         conn.commit()
@@ -17,6 +17,15 @@ def new_car_owner(owner_name, email, owner_type, contact_number):
 
     return False
 
+# Fetch All Car Owners
+def get_car_owners():
+    try:
+        cursor.execute("SELECT * FROM car_owners")
+        return cursor.fetchall()
+    except Exception as e:
+        print("Error Occurred!", e)
+        return None
+
 # Assign Vehicle to Car Owner
 def assign_vehicle(owner_id, plate_number, vehicle_type):
     try:
@@ -26,3 +35,4 @@ def assign_vehicle(owner_id, plate_number, vehicle_type):
         conn.commit()
     except Exception as e:
         print("Error Occurred!", e)
+
