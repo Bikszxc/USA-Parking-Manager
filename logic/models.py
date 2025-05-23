ed5c272c8a93c30356a51095e9225d87a8345b22
Bikzxcs <reagan2sotelo@gmail.com>
2025-05-23 21:47:15 +0800

    - added create reservation for FastAPI website form

--- logic/models.py
+++ logic/models.py
@@ -18,6 +18,24 @@ def new_admin_log(admin_name, action):
     except Exception as e:
         print(f"Error Occurred! {e}")
 
+def create_reservation(name, email, contact_number, plate_number, vehicle_type, reservation_date, reservation_time):
+    try:
+        api_conn = get_connection()
+        api_cursor = api_conn.cursor()
+
+        reservation_date = datetime.strptime(reservation_date, "%Y-%m-%d")
+        reservation_time = datetime.strptime(reservation_time, "%H:%M")
+
+        api_cursor.execute("INSERT INTO reservations (name, email, contact_number, plate_number, vehicle_type, reservation_date, reservation_time) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                       (name, email, contact_number, plate_number, vehicle_type, reservation_date.strftime("%m-%d-%Y"), reservation_time.strftime("%H:%M")))
+
+        api_conn.commit()
+
+        return True
+    except Exception as e:
+        print(f"Error Occurred! {e}")
+        return False
+
 # Add New Car Owner
 def new_car_owner(owner_name, email, owner_type, contact_number, refresh_callback=None) -> bool:
     try:
@@ -37,7 +55,7 @@ def new_car_owner(owner_name, email, owner_type, contact_number, refresh_callbac
 
 
 # Park Vehicle to a Parking Slot
-def park_vehicle(admin_name, slot_number, vehicle_type, owner_name, plate_number, status_type, contact_number) -> bool:
+def park_vehicle(slot_number, vehicle_type, owner_name, plate_number, status_type, contact_number) -> bool:
     try:
         cursor.execute('''
                        UPDATE parking_slots
@@ -52,7 +70,7 @@ def park_vehicle(admin_name, slot_number, vehicle_type, owner_name, plate_number
 
         conn.commit()
 
-        new_admin_log(admin_name, f"Parked {plate_number} in Slot {slot_number}")
+        # new_admin_log(admin_name, f"Parked {plate_number} in Slot {slot_number}")
 
         return True
     except Exception as e:
@@ -101,6 +119,19 @@ def assign_vehicle(owner_name, plate_number, vehicle_type, expiration_date) -> b
         print("Error Occurred!", e)
         return False
 
+def renew_vehicle(plate_number, expiration_date) -> bool:
+    try:
+        cursor.execute('''UPDATE registered_cars 
+                          SET registration_date = ?,
+                              expiration_date = ?
+                          WHERE plate_number = ?
+        ''', (ph_time.strftime("%m/%d/%Y"),  expiration_date, plate_number))
+        conn.commit()
+
+        return True
+    except Exception as e:
+        print(f"Error Occurred! {e}")
+        return False
 
 # ================== DELETION =================== #
 
@@ -118,6 +149,40 @@ def assign_vehicle(owner_name, plate_number, vehicle_type, expiration_date) -> b
 
 # ==================== FETCHES ===================== #
 
+def get_vehicles():
+    try:
+        cursor.execute("SELECT * FROM registered_cars")
+        return cursor.fetchall()
+    except Exception as e:
+        print(f"Error Occurred! {e}")
+        return None
+
+def get_vehicle_owner(owner_id):
+    try:
+        cursor.execute("SELECT owner_name FROM car_owners WHERE id = ?", (owner_id,))
+        owner_name = cursor.fetchone()
+
+        if owner_name:
+            return owner_name[0]
+
+        return None
+    except Exception as e:
+        print(f"Error Occurred! {e}")
+        return None
+
+def check_plate_number(plate_number) -> bool:
+    try:
+        cursor.execute("SELECT id FROM registered_cars WHERE plate_number =?", (plate_number,))
+        plate_number = cursor.fetchone()
+
+        if plate_number:
+            return True
+
+        return False
+    except Exception as e:
+        print(f"Error Occurred! {e}")
+        return False
+
 def record_found(owner_name: str = None):
     try:
         cursor.execute("SELECT * FROM car_owners WHERE owner_name = ?", (owner_name,))
@@ -130,6 +195,19 @@ def record_found(owner_name: str = None):
     except Exception as e:
         print("Error Occurred!", e)
         return False
+
+def get_reservations():
+    try:
+        cursor.execute("SELECT * FROM reservations")
+        reservations = cursor.fetchall()
+
+        if reservations:
+            return reservations
+
+        return None
+    except Exception as e:
+        print(f"Error Occurred! {e}")
+        return None
 
 
 def get_owner(owner_name: str):
@@ -155,7 +233,7 @@ def get_car_owners():
 # Fetch All Vehicles of Car Owner
 def get_owner_vehicles(owner_name):
     try:
-        cursor.execute("SELECT id FROM car_owners WHERE owner_name = ?", (owner_name,))
+        cursor.execute("SELECT id FROM car_owners WHERE owner_name = ? COLLATE NOCASE", (owner_name,))
         owner_id = cursor.fetchone()
         owner_id = owner_id[0]
 
